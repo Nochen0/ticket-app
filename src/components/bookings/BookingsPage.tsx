3dff8e6f888b803298f098610df609b1f54eaee2
Nochen0 <enesaydindev@gmail.com>
2025-11-03 06:33:20 +0300

    updated bookings page

--- src/components/bookings/BookingsPage.tsx
+++ src/components/bookings/BookingsPage.tsx
@@ -1,3 +1,28 @@
 export default function BookingsPage() {
-  return <div style={{ width: "calc(100vw - 290px)" }}>Bookings</div>
+  return (
+    <div style={{ width: "calc(100vw - 290px)" }}>
+      <h2>Bookings</h2>
+      <div>
+        <div>
+          <h3>List of bookings</h3>
+          <div>
+            <button>Mark ad used</button>
+            <button>+ Add booking</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input type="search" />
+            <button>Other filters...</button>
+          </div>
+          <div>
+            <p>Order by</p>
+            <button>Creation date</button>
+            <button>Order</button>
+          </div>
+        </div>
+        {/* table */}
+      </div>
+    </div>
+  )
 }
